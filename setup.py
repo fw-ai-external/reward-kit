ee7501d89b79f8b4a3297fa4c0e554ccfce827ab
Yinghan Ma <yinghan.ma@fireworks.ai>
2025-05-16 10:55:10 -0700

    bump up version to 0.2.11

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="reward-kit",
-    version="0.2.10",
+    version="0.2.11",
     packages=find_packages(),
     install_requires=[
         "requests>=2.25.0",
